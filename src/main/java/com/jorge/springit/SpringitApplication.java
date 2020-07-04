7116c509147c6cb57f6b44e78911a5aaf9609050
Jorge Villarreal <jorge.vi96@gmail.com>
2020-07-03 21:13:17 -0500

    Added views for links view and submit

--- src/main/java/com/jorge/springit/SpringitApplication.java
+++ src/main/java/com/jorge/springit/SpringitApplication.java
@@ -1,10 +1,6 @@
 package com.jorge.springit;
 
-import com.jorge.springit.model.Comment;
-import com.jorge.springit.model.Link;
-import com.jorge.springit.repository.CommentRepository;
-import com.jorge.springit.repository.LinkRepository;
-import org.springframework.boot.CommandLineRunner;
+import com.ocpsoft.pretty.time.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,19 +14,10 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
+
     @Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
-        return args -> {
-            Link link = new Link("Getting started with Spring Boot 2", "https://www.testlinkpage.com/spring-boot-2");
-            linkRepository.save(link);
-
-
-            Comment comment = new Comment("This Spring Boot 2 is awesome!", link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-
-            System.out.println("We just inserted a link and a comment");
-            System.out.println("=====================================");
-        };
+    PrettyTime prettyTime(){
+        return new PrettyTime();
     }
+
 }
--- src/main/java/com/jorge/springit/model/Link.java
+++ src/main/java/com/jorge/springit/model/Link.java
@@ -1,13 +1,22 @@
 package com.jorge.springit.model;
 
+import com.jorge.springit.service.BeanUtil;
+import com.ocpsoft.pretty.time.PrettyTime;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -22,14 +31,33 @@ public class Link extends Auditable{
     private Long id;
 
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
     private String title;
 
     @NonNull
+    @NotEmpty(message = "Please enter a url.")
+    @URL(message = "Please enter a valid url.")
     private String url;
 
     //Comments
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
+
+    public String getDomainName() throws URISyntaxException
+    {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime(){
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert){
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 
     public void addComment(Comment comment){
         this.comments.add(comment);
